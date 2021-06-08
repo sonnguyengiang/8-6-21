e5d4e89c5400701662735ae8f882631e02039c29
sonnguyengiang <sonstarx2212@gmail.com>
2021-06-08 15:28:14 +0700

    up

--- test/src/Test.java
+++ test/src/Test.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        int temp = 0, max = 0, min = 0;
-        for (int i = 1;i < number; i++) {
-            if (number % i == 0) {
-                max = i ==
-                }
-            }
-        }
+        int x = 5;
+
+        do{
+
+            System.out.println("“CodeGym”");
+
+            x--;
+
+        } while(x == 5);
     }
 }
+
