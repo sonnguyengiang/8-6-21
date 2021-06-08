092333c656053434cfb51b1d83dca9dbe7d6a9b5
sonnguyengiang <sonstarx2212@gmail.com>
2021-06-08 10:10:57 +0700

    up

---
+++ test/src/Test.java
@@ -0,0 +1,14 @@
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Moi ban nhap ky tu: ");
+        String character = sc.nextLine();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(character);
+        }
+    }
+}
